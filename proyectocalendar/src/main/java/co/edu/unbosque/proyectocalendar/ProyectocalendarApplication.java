f373185e6fb7ebbc6b1bfea37aba2c3b7c0f1227
JulianRH15 <julianrh734@gmail.com>
2025-04-30 10:29:36 -0500

    PROYECTO BACK CREADO
    Se ha creado el proyecto para el back

---
+++ proyectocalendar/src/main/java/co/edu/unbosque/proyectocalendar/ProyectocalendarApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.proyectocalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectocalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectocalendarApplication.class, args);
+	}
+
+}
